a829f12a66947e9866057fae8d250fef21e049b2
Andrew Watson <Andrew.justinw@gmail.com>
2021-09-22 13:40:06 -0400

    Completed login page MVP

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div className="login-page">
+        <div className="login">
+            <div className="login-wrapper">
+
+                <div className="login-top">
+
+                    <h3 className="login-logo">CyberSocial</h3>
+                    <span className="login-cta">
+                        Connect with friends and the world around you on CyberSocial.
+                    </span>
+
+                </div>
+
+                <div className="login-bottom">
+
+                    <div className="login-box">
+
+                        <input placeholder="Email" className="login-input" />
+                        <input placeholder="Password" className="login-input" />
+
+                        <button className="login-button">Log In</button>
+                        <span className="login-forgot">Forgot Password?</span>
+
+                        <button className="login-register-button">
+                            Create a New Account
+                        </button>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
